71c693a20f08ac387bcfd06f110dab522949a5db
Rinat Milenskii <milenskikills@gmail.com>
2025-08-13 10:09:18 -0300

    use memo task

--- src/components/FilteredList.tsx
+++ src/components/FilteredList.tsx
@@ -5,14 +5,29 @@ const items = Array.from({ length: 1000 }, (_, i) => `Элемент ${i + 1}`);
 export default function FilteredList() {
   const [filter, setFilter] = useState("");
 
+  const filtredElement = useMemo(() => {
+    return items.filter((items) =>
+      items.toLowerCase().includes(filter.toLowerCase())
+    );
+  }, [filter]);
+
   return (
     <div>
       <input
-        placeholder="Поиск..."
+        placeholder="Searching..."
         value={filter}
         onChange={(e) => setFilter(e.target.value)}
       />
-      <ul>{/* Отобразить отфильтрованные элементы */}</ul>
+      <ul>
+        {filtredElement.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+// Задача:
+// Есть список из 1000 элементов. Добавь поле поиска,
+// которое фильтрует список. Фильтрация должна происходить
+// эффективно, без лишних вычислений.

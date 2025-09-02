c014585ede919e8d18ec3d10b6643d2808762fde
Rinat Milenskii <milenskikills@gmail.com>
2025-09-02 11:30:28 -0300

    fizzBuzz

--- src/components/FizzBuzz.tsx
+++ src/components/FizzBuzz.tsx
@@ -1,30 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function FizzBuzz() {
   const [num, setNum] = useState(0);
-  const [] = useState([])
+  const [visibleArray, setVisibleArray] = useState<(string | number)[]>([]);
+  useEffect(() => {
+    const resArray = [];
 
-  useEffect (() => {
-	if number === 5 
-		fskldlfsldfl
-	if else number === 3
-		fkdskfsdkfk
-	if numebr 3 || 5 
-		fksdlfls
-	else 
+    for (let i = 0; i <= num; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        resArray.push("FizzBuzz");
+      } else if (i % 3 == 0) {
+        resArray.push("Fizz");
+      } else if (i % 5 == 0) {
+        resArray.push("Buzz");
+      } else {
+        resArray.push(i);
+      }
+    }
+    setVisibleArray(resArray);
+  }, [num]);
 
-  }, [])
-const ar = []
-  for (let i = 0; i <= 5; i++) {
-	if 1231 == 3
-		ar.push('Fizz')
-	ar.push(i);
-  }
   return (
     <div>
-      <input type="number" onChange={} />
-	    <p >Entered number: {5}</p>
-	    <div>{здесь выводим список в любом удобном формате}</div>
+      <input
+        type="number"
+        onChange={(e) => {
+          const valueNumber = Number(e.target.value);
+          setNum(valueNumber);
+        }}
+      />
+      <p>Entered number: {num}</p>
+      <div>
+        {visibleArray.map((element) => (
+          <p>{element}</p>
+        ))}
+      </div>
     </div>
   );
 }
